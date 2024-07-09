fd7e4bae1b87e8e500a58f82dfa9638402893fc9
Tanveer Ahmed <tanveerahmed85820@gmail.com>
2024-07-09 09:59:07 +0530

    learning context api

---
+++ Context-Api/context-Api/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import UserContext from "../context/UserContext";
+import React from "react";
+
+function Profile() {
+  const { user } = useContext(UserContext);
+
+  if (!user) return <div>Please login</div>;
+
+  return <div>Welcome {user.username}</div>;
+}
+
+export default Profile;
